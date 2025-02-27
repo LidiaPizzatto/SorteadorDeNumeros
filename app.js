8c430a699b21e3ca4f182691de24ea2a9f4f3c07
Lidia Pizzatto <lidiacfp@gmail.com>
2025-02-27 17:39:54 -0300

    figura e texto

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function reiniciar() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
-    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">OS NÚMEROS APARECERAM AQUI</label>';
     statusBotaoReiniciar();
 }
 
